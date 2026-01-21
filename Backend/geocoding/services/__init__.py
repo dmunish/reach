a0f3f4e19c02c5175d9fcf456fa9d1eec944986e
Ahmad Shahmeer <ahmadshahmeer26@gmail.com>
2026-01-21 23:22:43 +0500

    feat: export redis_cache module from services package

--- Backend/geocoding/services/__init__.py
+++ Backend/geocoding/services/__init__.py
@@ -6,11 +6,15 @@ from .geocoding_service import GeocodingService
 from .name_matcher import NameMatcher
 from .external_geocoder import ExternalGeocoder
 from .directional_parser import DirectionalParser, Direction
+from .redis_cache import RedisCache, get_redis_cache, set_redis_cache
 
 __all__ = [
     'GeocodingService',
     'NameMatcher',
     'ExternalGeocoder',
     'DirectionalParser',
-    'Direction'
+    'Direction',
+    'RedisCache',
+    'get_redis_cache',
+    'set_redis_cache'
 ]

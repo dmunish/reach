6d81b090998475a5be7d1a0dcbe1052511233de6
dmunish <thenish2004@gmail.com>
2026-04-02 00:26:44 +0500

    Removed because State has been massively simplified

--- Backend/agents/state.py
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict, Annotated, Sequence
-from langchain_core.messages import BaseMessage
-from operator import add
-
-class State(TypedDict):
-    messages: Annotated[Sequence[BaseMessage], add]
-    db_results: dict
-    iteration_count: int
-    is_complete: bool
